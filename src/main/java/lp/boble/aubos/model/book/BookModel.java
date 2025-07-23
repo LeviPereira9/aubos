e05776288a2959a191d1dc86fe904317218460c2
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-07-23 15:49:57 -0300

    feat: livro schema

--- src/main/java/lp/boble/aubos/model/book/BookModel.java
+++ src/main/java/lp/boble/aubos/model/book/BookModel.java
@@ -1,4 +1,66 @@
 package lp.boble.aubos.model.book;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lp.boble.aubos.model.book.dependencies.*;
+import lp.boble.aubos.model.user.UserModel;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_book")
+@Data
 public class BookModel {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+    @Column(name = "subtitle")
+    private String subtitle;
+    @Column(name = "synopsis")
+    private String synopsis;
+    @Column(name = "published_at")
+    private LocalDate publishedAt;
+    @Column(name = "finished_at")
+    private LocalDate finishedAt;
+
+    //Relações
+    @ManyToOne
+    @JoinColumn(name = "language_id")
+    private LanguageModel language;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private Type type;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "restriction_id")
+    private Restriction restriction;
+
+    @ManyToOne
+    @JoinColumn(name = "license_id")
+    private LicenseModel licenseModel;
+
+    // Controle
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private UserModel createdBy;
+
+    @Column(name = "last_update")
+    private Instant lastUpdated;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_by")
+    private UserModel updatedBy;
+
+    // Relações N:N
+
 }
---
+++ src/main/resources/db/migration/V202507231530__alter_table_book_redudant_time.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tb_book MODIFY COLUMN published_at DATE;
+ALTER TABLE tb_book MODIFY COLUMN finished_at DATE;
