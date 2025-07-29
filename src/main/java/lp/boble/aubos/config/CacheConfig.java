18a24e3725d13b61493a6f2cef68f9dccead6e12
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-07-29 14:22:40 -0300

    fix: cache da busca dos livro está funcionando corretamente

--- src/main/java/lp/boble/aubos/config/CacheConfig.java
+++ src/main/java/lp/boble/aubos/config/CacheConfig.java
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
-import org.flywaydb.core.api.callback.Warning;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -15,6 +11,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.util.PathMatcher;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -24,9 +21,14 @@ import java.util.Map;
 @Slf4j
 public class CacheConfig {
 
+    RedisCacheConfiguration customConfig(Duration ttl, ObjectMapper mapper) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(mapper)))
+                .entryTtl(ttl);
+    }
 
     @Bean
-    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory, PathMatcher mvcPathMatcher) {
 
         try {
             connectionFactory.getConnection().ping();
@@ -39,19 +41,15 @@ public class CacheConfig {
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        // Serialização + TTL de 1 Hora.
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                        new GenericJackson2JsonRedisSerializer(mapper)))
-                .entryTtl(Duration.ofHours(1));
+        RedisCacheConfiguration defaultConfig = customConfig(Duration.ofHours(1), mapper);
 
         // TTL customizados
         Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-        cacheConfigurations.put("bookSearch", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)));
-        cacheConfigurations.put("userSearch", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+        cacheConfigurations.put("bookSearch", customConfig(Duration.ofMinutes(10),mapper));
+        cacheConfigurations.put("userSearch", customConfig(Duration.ofMinutes(10),mapper));
 
         return RedisCacheManager.builder(connectionFactory)
-                .cacheDefaults(configuration)
+                .cacheDefaults(defaultConfig)
                 .withInitialCacheConfigurations(cacheConfigurations)
                 .build();
     }
