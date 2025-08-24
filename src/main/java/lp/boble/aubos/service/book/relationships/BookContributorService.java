05a9638fc026db4521baf8d8455a44b4d485e844
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-08-24 11:56:20 -0300

    BookContributorService init

---
+++ src/main/java/lp/boble/aubos/service/book/relationships/BookContributorService.java
@@ -0,0 +1,11 @@
+package lp.boble.aubos.service.book.relationships;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// TODO: GetSpecificsContributors
+// TODO: Add Contributor to Book, Single or Batch
+// TODO: Remove Contributor from Book, Single or Batch
+@Service
+@RequiredArgsConstructor
+public class BookContributorService {}
