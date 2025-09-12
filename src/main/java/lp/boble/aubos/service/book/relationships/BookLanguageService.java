f897cd6f062f9ef07e2c94b079af2a89edc0391e
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-09-12 15:05:22 -0300

    feat: init book language service

---
+++ src/main/java/lp/boble/aubos/service/book/relationships/BookLanguageService.java
@@ -0,0 +1,8 @@
+package lp.boble.aubos.service.book.relationships;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookLanguageService {}
