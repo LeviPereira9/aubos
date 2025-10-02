b29178201592d2f13ba2252de87e48f98e7f0c23
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-10-02 09:11:14 -0300

    feat: cascade nas relações

--- src/main/java/lp/boble/aubos/model/book/dependencies/TagModel.java
+++ src/main/java/lp/boble/aubos/model/book/dependencies/TagModel.java
@@ -2,6 +2,10 @@ package lp.boble.aubos.model.book.dependencies;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lp.boble.aubos.model.book.relationships.BookTagModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_tag")
@@ -13,4 +17,6 @@ public class TagModel {
 
     private String name;
 
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookTagModel> bookTags;
 }
