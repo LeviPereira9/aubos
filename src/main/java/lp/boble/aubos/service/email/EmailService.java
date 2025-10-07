b2414b86e0d35c72dfbf76faa77a96161d7d3f07
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-10-07 16:49:18 -0300

    fix: email timeout

--- src/main/java/lp/boble/aubos/service/email/EmailService.java
+++ src/main/java/lp/boble/aubos/service/email/EmailService.java
@@ -1,9 +1,15 @@
 package lp.boble.aubos.service.email;
 
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lp.boble.aubos.exception.custom.email.CustomEmailException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +18,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    public void simpleEmail(String email, String subject, String text) {
+    private void sendEmail(String email, String subject, String text) {
         try{
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(email);
@@ -24,44 +30,19 @@ public class EmailService {
         }
     }
 
-    /**
-     * Envia o código de confirmação de e-mail, para o e-mail do sujeito.
-     * @param email do usuário
-     * @param token de ativação
-     * @throws CustomEmailException Em caso de: <br>
-     * - Algum erro no envio.
-     *
-     * */
+    @Async
     public void sendVerifyEmail(String email, String token){
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(email);
-            message.setSubject("[Aubos]: Verificação de E-mail.");
-            message.setText(String.format("Código para confirmação do e-mail: %s", token));
-            mailSender.send(message);
-        } catch (Exception e){
-            throw CustomEmailException.failed();
-        }
+        String subject = "[Aubos]: Verificação de E-mail.";
+        String text = String.format("Código para confirmação do e-mail: %s", token);
+
+        this.sendEmail(email, subject, text);
     }
 
-    /**
-     * Envia o código de reset de senha, para o e-mail do sujeito.
-     * @param email do usuário
-     * @param token de ativação
-     * @throws CustomEmailException Em caso de: <br>
-     * - Algum erro no envio.
-     *
-     * */
+    @Async
     public void sendPasswordResetEmail(String email, String token){
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(email);
-            message.setSubject("[Aubos]: Recuperação de senha.");
-            message.setText(String.format("Código para recuperação de senha: %s", token));
-            mailSender.send(message);
-        } catch (Exception e){
-            throw CustomEmailException.failed();
-        }
+            String subject = "[Aubos]: Recuperação de senha.";
+            String text = String.format("Código para recuperação de senha: %s", token);
+            this.sendEmail(email, subject, text);
     }
 
 }
